0d94659751142c1f800ec621b66014002fdf3b06
JamesWyattMartin <38168959+JamesWyattMartin@users.noreply.github.com>
2020-07-13 13:55:46 -0500

    Update first.js

--- first.js
+++ first.js
@@ -2,6 +2,6 @@
 /* this is just like other languages 
 using a multiline comment*/
 
-let harry = {name: 'Harry Houdini', age: 64}
+let harry = {name: 'David Coperfield', age: 64}
 console.log(harry.name)
 console.log(harry.age)
